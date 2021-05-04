0dd62168f84ae6416f7959609ec0340488c3074f
Pascal Eberlein <pascal@eberlein.io>
2021-05-04 21:12:39 +0200

    update

--- pyug/__main__.py
+++ pyug/__main__.py
@@ -1,11 +1,15 @@
-from pyug import get_random_name
+from pyug import get_random_username
 from argparse import ArgumentParser
 
 
-if __name__ == '__main__':
+def main():
     ap = ArgumentParser()
     ap.add_argument("-c", "--count", help="Count of random names to print", default=1)
     a = ap.parse_args()
 
     for _ in range(a.count):
-        print(get_random_name())
+        print(get_random_username())
+
+
+if __name__ == '__main__':
+    main()
